91f326da6e889dfb50ed51ebf5f5394deac1c32b
tobiiu <tobiaspfeil5@gmail.com>
2025-06-05 00:20:39 +0200

    4

--- server.js
+++ server.js
@@ -2,32 +2,22 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const socket = new WebSocket('wss://multiplayerv1-production.up.railway.app');
-
-
-const indexPath = path.join(__dirname, 'public/index.html');
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(indexPath);
-});
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
   ws.on('message', (data) => {
     const message = data.toString();
-    console.log('📡 Nachricht erhalten vom Client:', message);
+    console.log('Nachricht erhalten:', message);
 
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
+    wss.clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
       }
     });
@@ -42,4 +32,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server läuft auf http://0.0.0.0:${PORT}`);
 });
-
